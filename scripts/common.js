a554d51a12294af3041975e90606270d56334830
Bobby Lin <bobbylinjh@gmail.com>
2015-12-20 15:48:27 +0800

    Fixed timer bug of startTimer() in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -5,43 +5,52 @@
 
 function setToFiftyMins() {
     clearInterval(timer);
+    timer = -1;
     $("#defaultTime").html("50:00");
 }
 
 function setToTwentyFiveMins() {
     clearInterval(timer);
+    timer = -1;
     $("#defaultTime").html("25:00");
 }
 
 function setToShortBreak() {
     clearInterval(timer);
+    timer = -1;
     $("#defaultTime").html("05:00");
 }
 
 function setToLongBreak() {
     clearInterval(timer);
+    timer = -1;
     $("#defaultTime").html("10:00");
 }
 
-var timer;
+var timer = -1;
 
 function startTimer() {
-    startCountdown();
+    if(timer === -1)
+        startCountdown();
+    console.log("started " + timer);
 }
 
 function pauseTimer() {
     clearInterval(timer);
+    timer = -1;
+    console.log("paused");
 }
 
 function reset() {
     clearInterval(timer);
+    timer = -1;
     $("#defaultTime").html("25:00");
     console.log("reset to default");
 }
 
 function startCountdown() {
     var e = document.getElementById("defaultTime");
-    timer = setInterval(decrement, 1);
+    timer = setInterval(decrement, 1000);
     
     function decrement() {
         var arr =  e.innerHTML.split(":");
