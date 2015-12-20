6cfb82abb0eddaaef9a0ff05992d9c94d1fbd299
Bobby Lin <bobbylinjh@gmail.com>
2015-12-20 16:10:18 +0800

    Refactored to refreshTimer() in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -4,46 +4,46 @@
  */
 
 function setToFiftyMins() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     $("#defaultTime").html("50:00");
 }
 
 function setToTwentyFiveMins() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     $("#defaultTime").html("25:00");
 }
 
 function setToShortBreak() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     $("#defaultTime").html("05:00");
 }
 
 function setToLongBreak() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     $("#defaultTime").html("10:00");
 }
 
 var timer = -1;
 
+function refreshTimer() {
+    clearInterval(timer);
+    timer = -1;
+}
+
 function startTimer() {
-    if(timer === -1)
+    if(timer === -1) {
         startCountdown();
+    }
     console.log("started " + timer);
 }
 
 function pauseTimer() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     console.log("paused");
 }
 
 function reset() {
-    clearInterval(timer);
-    timer = -1;
+    refreshTimer();
     $("#defaultTime").html("25:00");
     console.log("reset to default");
 }
